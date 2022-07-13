8091ad31db80d08f89d99e03c34e876ee1e1798d
Owen <owencorrigan76@gmail.com>
2022-07-13 11:35:09 +0100

    Controller method refactored

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -14,13 +14,19 @@ import static java.lang.Thread.sleep;
 public class Controller {
 
     public static void main(String[] args) throws InterruptedException {
-        
-       KafkaProducerClass myProducer= new KafkaProducerClass(){};
-       KafkaConsumerClass myConsumer = new KafkaConsumerClass();
 
-      
-       myProducer.producing();
-       myConsumer.consuming();
+        if (args[0].equals("producer")) { // object literal    calling .equals and passing it producer
+            System.out.println("Starting producer");
+            KafkaProducerClass myProducer = new KafkaProducerClass();
+            myProducer.producing();
+        } else if (args[0].equals("consumer")) {
+            System.out.println("Starting consumer");
+            KafkaConsumerClass myConsumer = new KafkaConsumerClass();
+            myConsumer.consuming();
+        } else {
+            throw new IllegalArgumentException("Unknown Argument: " + args[0] + " inputted.");
+        }
     }
 }
+
 
